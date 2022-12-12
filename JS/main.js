7c78cfbbf9d89fcd9fc70e6cf78a32c35f13fa9c
Alon <alonzso07@gmail.com>
2022-12-12 14:02:18 -0600

    se realizo la rama develop y se corrigio el programa

--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,16 @@
-var cadena = "Cuantas 'A' aparecen en esta cadena."
-
-var indices = [];
-for(var i = 0; i < cadena.length; i++) {
-	if (cadena[i].toLowerCase() === "a") indices.push(i);
+let cadena = "almazan";
+cadena=cadena.toLowerCase();
+let letra="";
+let contarfin=0;
+for (let index = 0; index < cadena.length; index++) {
+   let temporal=0;
+   for (let i = 0; i < cadena.length; i++) {
+ if (cadena.charAt(index)== cadena.charAt(i))
+    temporal++;  
+   } 
+   if (temporal>contarfin) {
+    contarfin=temporal;
+    letra=cadena.charAt(index);
+   }
 }
-console.log(`la "A" se repite ${indices.length} veces`);
+console.log("En la cadena: "+cadena+"\nla letra: "+letra+" se repitio: "+contarfin+" veces");
